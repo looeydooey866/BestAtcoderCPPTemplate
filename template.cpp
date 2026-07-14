b69e4c9fa08e281cfdedcb7f1b82060996a66359
looeydooey866 <looeydooey866@gmail.com>
2026-07-14 17:17:43 +0800

    few more quotes

--- template.cpp
+++ template.cpp
@@ -4,8 +4,14 @@
 // 2. 99% of wrong solutions can be fixed by using #define int _int128
 // 3. if your solution is too slow, buy a computer with faster compute speed
 // 4. if not possible, make a donation to AtCoder Inc™ instead to improve their servers
-// 5. always use functions to organise your code, for example, write void take_integer_input() { cin >> n } instead of using cin >> n directly
-
+// 5. use commenst on every line. no comment = no understand. how do you expect others to read your code?
+// 6. always use functions to organise your code, for example, write void take_integer_input() { cin >> n } instead of using cin >> n directly
+// 7. if you can't find a determinstic solution, always fallback to simulated annealing
+// 8. if a scam doesn't exist, skip the problem (it's unsolvable)
+// 9. use ints instead of floats in order to fix precision. an integer x represents the float f * 10e(precision).
+// 10. never use this technique called "dynamic programming". it is too complicated and easy to get wrong.
+// 11. never write your own binary search. instead, rely on c++'s upper/lower bound functions
+// 12. always use templating for trivially known function parameters. the 1% performance boost is critical
 
 #include <bits/stdc++.h>
 using namespace std;
